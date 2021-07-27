c745f58a298a392de1e4e2be19eb8e92fc84be92
Marcos Kirchner <marcoskirchner@live.com>
2021-07-27 20:55:19 -0300

    Fourth failed attempt.

--- Kattis/Sequences.cs
+++ Kattis/Sequences.cs
@@ -4,11 +4,14 @@ namespace Kattis
 {
 
     /*
-    Third attempt at 0-1 Sequences, actually a sanity check.
+    Fourth attempt at 0-1 Sequences, using recursion.
 
-    The second submission also failed, but it looked so rightish. Let's get rid of the code that handles expanding ? into different sequences.
+    The third submission proved we can fail before the third test, so we were definitely doing something right. Actually, this input:
+    0010001101100001000010?1011010111101110100001101?110000100111100?101?001011111011?01000101110001000?001110?10111?00100111100110?11010000010?100110110110110101000?01000?01010100101101100??011011??10110111101011?11?10001000001010011?1?0?01000?00111101101100111001100??1111101100100100000001001001101
 
-    As expected, this try fails on the first test.
+    Produces -503.316.480. Negatives are a big no-no here. We are overflowing our ints. A checked block confirmed that. So we just return a long, right?
+    
+    Still got Wrong Answer on the third test.
      */
 
     public class Sequences
@@ -16,14 +19,25 @@ namespace Kattis
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-            Console.WriteLine(Inversions(input, 0, 0, 0) % 1000000007);
+            Console.WriteLine(Inversions(input));
         }
 
-        private static int Inversions(string input, int pos, int swaps, int ones)
+        private static long Inversions(string input)
+        {
+            return Inversions(input, 0, 0, 0) % 1000000007;
+        }
+
+        private static long Inversions(string input, int pos, int swaps, int ones)
         {
             for (int i = pos; i < input.Length; i++)
             {
-                if (input[i] == '0')
+                if (input[i] == '?')
+                {
+                    return
+                        Inversions(input, i + 1, swaps + ones, ones)
+                        + Inversions(input, i + 1, swaps, ones + 1);
+                }
+                else if (input[i] == '0')
                 {
                     swaps += ones;
                 }
