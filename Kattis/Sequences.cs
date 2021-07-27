64ad32c7d10890b3ab5d99d5066c8b3a975473ff
Marcos Kirchner <marcoskirchner@live.com>
2021-07-27 20:44:31 -0300

    Third failed attempt.

--- Kattis/Sequences.cs
+++ Kattis/Sequences.cs
@@ -4,14 +4,11 @@ namespace Kattis
 {
 
     /*
-    Second attempt at 0-1 Sequences, using recursion.
+    Third attempt at 0-1 Sequences, actually a sanity check.
 
-    Since the first submission failed, something must be of. Testing with a larger input:
-    0010001101100001000010?1011010111101110100001101?110000100111100?101?001011111011?01000101110001000?001110?10111?00100111100110?11010000010?100110110110110101000?01000?01010100101101100??011011??10110111101011?11?10001000001010011?1?0?01000?00111101101100111001100??11111011001001000000010010011
-    
-    Produces the output 1.358.954.496. Aren't we supposed to output the number of inversions module 1.000.000.007? Let's try it
+    The second submission also failed, but it looked so rightish. Let's get rid of the code that handles expanding ? into different sequences.
 
-    This submission also passes the first two tests but fails the third with Wrong Answer. Clearly there's more to do.
+    As expected, this try fails on the first test.
      */
 
     public class Sequences
@@ -26,13 +23,7 @@ namespace Kattis
         {
             for (int i = pos; i < input.Length; i++)
             {
-                if (input[i] == '?')
-                {
-                    return
-                        Inversions(input, i + 1, swaps + ones, ones)
-                        + Inversions(input, i + 1, swaps, ones + 1);
-                }
-                else if (input[i] == '0')
+                if (input[i] == '0')
                 {
                     swaps += ones;
                 }
