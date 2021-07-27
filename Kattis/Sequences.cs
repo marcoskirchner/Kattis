e6aa6ff2094ddf56909428516069b2fec6bad19b
Marcos Kirchner <marcoskirchner@live.com>
2021-07-27 20:40:41 -0300

    Second failed attempt.

--- Kattis/Sequences.cs
+++ Kattis/Sequences.cs
@@ -4,18 +4,14 @@ namespace Kattis
 {
 
     /*
-    First attempt at 0-1 Sequences, using recursion.
+    Second attempt at 0-1 Sequences, using recursion.
 
-    The program looks correct. The sample test passes.
-    Some manual tests looks good:
-    10?10 - 8
-    1?010 - 9
-    10?10?10 - 37
+    Since the first submission failed, something must be of. Testing with a larger input:
+    0010001101100001000010?1011010111101110100001101?110000100111100?101?001011111011?01000101110001000?001110?10111?00100111100110?11010000010?100110110110110101000?01000?01010100101101100??011011??10110111101011?11?10001000001010011?1?0?01000?00111101101100111001100??11111011001001000000010010011
+    
+    Produces the output 1.358.954.496. Aren't we supposed to output the number of inversions module 1.000.000.007? Let's try it
 
-    A "larger" test produces a promising result:
-    1010?010111110110101000?11000111000?01100111010110111001?1000 - 7944
-
-    This submission passes the first two tests but fails the third with Wrong Answer.
+    This submission also passes the first two tests but fails the third with Wrong Answer. Clearly there's more to do.
      */
 
     public class Sequences
@@ -23,7 +19,7 @@ namespace Kattis
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-            Console.WriteLine(Inversions(input, 0, 0, 0));
+            Console.WriteLine(Inversions(input, 0, 0, 0) % 1000000007);
         }
 
         private static int Inversions(string input, int pos, int swaps, int ones)
