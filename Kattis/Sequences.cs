30b48b0c3e95a4ab3c1ce48f1622eb7278752a5e
Marcos Kirchner <marcoskirchner@live.com>
2021-07-28 16:51:56 -0300

    Seventh failed attempt.

--- Kattis/Sequences.cs
+++ Kattis/Sequences.cs
@@ -1,19 +1,19 @@
 ﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace Kattis
 {
 
     /*
-    Sixth attempt at 0-1 Sequences, using smarter logic.
+    Seventh attempt at 0-1 Sequences, using smarter logic and BigIntegers.
 
-    When thinking about improving the recursive version, the outset didn't look that great.
-    A "mere" input with length of about 300 chars took a couple seconds to process.
-    The problem states we can have a input of 500.000 chars. That will take forever. And our allowance is only 1s.
+    Now we are overflowing a long. That's a big number but still. Let's bring out those heavy BigInteger guns.
 
     So let's try doing a single pass over the input string and no recursion at all.
     
-    We still got the first 14 tests right and failed on 15th. We are back at a Wrong Asnswer again.
-    But we got it wrong in 0.03s. Talk about failing fast. Isn't that what Agile is all about?
+    We are back at Time Limit Exceeded on that 15th test again. Seems like BigInteger is slow.
      */
 
     public class Sequences
@@ -26,9 +26,9 @@ namespace Kattis
 
         private static long Inversions(string input)
         {
-            long swaps = 0;
-            long ones = 0;
-            var seqs = 1;
+            BigInteger swaps = 0;
+            BigInteger ones = 0;
+            BigInteger seqs = 1;
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] == '?')
@@ -47,7 +47,12 @@ namespace Kattis
                     ones += seqs;
                 }
             }
-            return swaps % 1000000007;
+            if (swaps > 1000000007)
+            {
+                swaps %= 1000000007;
+            }
+
+            return (long)swaps;
         }
     }
 }
